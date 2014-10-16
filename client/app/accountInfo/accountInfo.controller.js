ddb4d0f02f714f9cefc40b18d2cd102113937f53
zhoux616 <zhoux616@morris.umn.edu>
2014-10-16 10:03:55 -0500

    make info show up only after logged in.

--- client/app/accountInfo/accountInfo.controller.js
+++ client/app/accountInfo/accountInfo.controller.js
@@ -3,6 +3,7 @@
 angular.module('ursaMajorApp')
   .controller('AccountinfoCtrl', function ($scope, Auth) {
 
+    $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.getCurrentUser = Auth.getCurrentUser;
     $scope.getCurrentEmail = Auth.email;
 
