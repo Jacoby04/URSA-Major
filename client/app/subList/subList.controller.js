cf812a1db818944f9275efe3abb604a2b2888c5c
lehet005 <lehet005@morris.umn.edu>
2014-10-28 11:08:29 -0500

    Finished the view for the submission list. Now, everything can be read in an easy way!

--- client/app/subList/subList.controller.js
+++ client/app/subList/subList.controller.js
@@ -4,35 +4,32 @@ angular.module('ursaMajorApp')
     .controller('SublistCtrl', function ($scope) {
         $scope.message = 'Hello';
 
+        $scope.subs = {sub:[{
+            name:"",
+            email:"",
+            awesome:""
+        }]};
+
+        $(document).ready(function() {
+            $(function listStuff() {
+                $.getJSON("https://spreadsheets.google.com/feeds/list/1VH6bDj7m04P9AI5WmgKYfdfXAZYRbVFiSRPJ3r_bou4/od6/public/values?alt=json-in-script&callback=?",
+                    function (data) {
+                        $.each(data.feed.entry, function (i, entry) {
+                            var sub = '<span style="display:none">' + entry.id.$t + '</span>';
+                            var email = entry.gsx$username.$t;
+                            var name = entry.gsx$yourname.$t;
+                            var awesome = entry.gsx$areyouawesome.$t;
+                            $scope.subs.sub.push({
+                                name: name,
+                                email: email,
+                                awesome: awesome
+                            })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                        });
+                    });
+            });
+        });
 
     });
 
