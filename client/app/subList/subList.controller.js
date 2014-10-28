f7cb8cbe1d9cdbb661b82f8c73a6f7bdb33fa0b6
zhoux616 <zhoux616@morris.umn.edu>
2014-10-27 21:00:06 -0500

    Still working the view and organizing the submissions.

--- client/app/subList/subList.controller.js
+++ client/app/subList/subList.controller.js
@@ -3,34 +3,36 @@
 angular.module('ursaMajorApp')
     .controller('SublistCtrl', function ($scope) {
         $scope.message = 'Hello';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     });
 
-   /* var Spreadsheet = require('edit-google-spreadsheet');
-
-    Spreadsheet.load({
-        debug: true,
-        spreadsheetId: '11UhreZn8ZcrZ1LSWJz2s1PWEdHcjhuNkVn7w_yPfTFc',
-        worksheetName: 'Form Responses 1',
-
-        oauth: {
-            email: '210473020739-lmmpcdsvqukuup09e75c7ohjfc3jlll4@developer.gserviceaccount.com',
-            key: process.env.PEM_KEY="/downloads/URSA-Major-4a6da205d6e5.pem"
-        }
-
-    }, function sheetReady(err, spreadsheet) {
-
-        if (err) {
-            throw err;
-        }
-
-        spreadsheet.receive(function (err, rows, info) {
-            if (err) {
-                throw err;
-            }
-
-            var rows = ("Found rows:", rows);
-
-            //console.dir(rows);
-            //console.dir(info);
-        });
-        */
