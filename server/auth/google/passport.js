6d4ff91d393ad9e64d5e427263672a1b08a7d213
DjGusaas <gusaa004@morris.umn.edu>
2014-10-28 16:07:54 -0500

    Restricted the login to only allow emails with the domain of morris.umn.edu

--- server/auth/google/passport.js
+++ server/auth/google/passport.js
@@ -38,4 +38,16 @@ exports.setup = function (User, config) {
       }
     }
   ));
+//    passport.use(new GoogleStrategy({
+//      clientID: config.google.clientID,
+//      clientSecret: config.google.clientSecret,
+//      callbackURL: config.google.callbackURL
+//    }, function(token, refreshToken, profile, done){
+//        if(profile._json.hd === "morris.umn.edu") {
+//            return done;
+//        }else{
+//            done(new Error("Need a Morris x500 to access page"));
+//        }
+//    }));
+
 };
