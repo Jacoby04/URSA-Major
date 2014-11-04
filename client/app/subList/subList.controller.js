51057269fbbb19ecb6993748edd8e64c327a7baa
thele116 <thele116@morris.umn.edu>
2014-11-03 21:17:09 -0600

    Misc. small fixes/changes.

--- client/app/subList/subList.controller.js
+++ client/app/subList/subList.controller.js
@@ -1,34 +1,20 @@
 'use strict';
 
 angular.module('ursaMajorApp')
-    .controller('SublistCtrl', function ($scope) {
+    .controller('SublistCtrl', function ($scope, $http, Auth) {
+        $scope.isLoggedIn = Auth.isLoggedIn;
 
-        $scope.subs = {sub:[{
-            name:"",
-            email:"",
-            awesome:""
-        }]};
+        $scope.jsonSource = "https://spreadsheets.google.com/feeds/list/1ImSQ0fy65Bc9NjmgHrpruaDrodC2uJ1n4RYl2OTX9Po/od6/public/values?alt=json";
+        $scope.localData = [];
 
-        $(document).ready(function() {
-            $(function listStuff() {
-                $.getJSON("https://spreadsheets.google.com/feeds/list/1VH6bDj7m04P9AI5WmgKYfdfXAZYRbVFiSRPJ3r_bou4/od6/public/values?alt=json-in-script&callback=?",
-                    function (data) {
-                        $.each(data.feed.entry, function (i, entry) {
-                            var sub = '<span style="display:none">' + entry.id.$t + '</span>';
-                            var email = entry.gsx$username.$t;
-                            var name = entry.gsx$yourname.$t;
-                            var awesome = entry.gsx$areyouawesome.$t;
-                            $scope.subs.sub.push({
-                                name: name,
-                                email: email,
-                                awesome: awesome
-                            })
-
-
-                        });
-                    });
+        $scope.updateLocalData = function(){
+            $http({method:'GET', url: $scope.jsonSource}).success(function(data) {
+                angular.forEach(data.feed.entry, function(value){
+                    $scope.localData.push(value);
+                });
             });
-        });
+        };
 
+        $scope.updateLocalData();
     });
 
