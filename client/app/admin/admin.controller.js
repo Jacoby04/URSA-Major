bb1c60101e4a1fda109376575ecd65f79fb779f4
finze008 <mpsmitch@gmail.com>
2014-11-05 12:50:10 -0600

    Added user role update system

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -6,7 +6,17 @@ angular.module('ursaMajorApp')
     // Use the User $resource to fetch all users
     $scope.users = User.query();
 
-    $scope.delete = function(user) {
+        $scope.roleOptions =
+            [   'Student',
+                'Review Group 1',
+                'Review Group 2',
+                'Committee Chair'
+            ];
+        $scope.role =
+            [""];
+
+
+        $scope.delete = function(user) {
       User.remove({ id: user._id });
       angular.forEach($scope.users, function(u, i) {
         if (u === user) {
@@ -14,4 +24,11 @@ angular.module('ursaMajorApp')
         }
       });
     };
+
+        $scope.changeRole = function(user) {
+            console.log(user);
+            if(confirm('Are you sure you want to update this users role?')) {
+                Auth.changeRole(user.role, user);
+            };
+        };
   });
