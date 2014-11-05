36267af1c64b1f97df072ac322ffcfebd530ff46
Jacoby04 <opdah023@morris.umn.edu>
2014-11-05 18:05:21 -0500

    Made it so an admin cannot take themselves off of an admin role.

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -3,8 +3,8 @@
 angular.module('ursaMajorApp')
   .controller('AdminCtrl', function ($scope, $http, Auth, User, $location) {
 
-    // Use the User $resource to fetch all users
-    $scope.users = User.query();
+        // Use the User $resource to fetch all users
+        $scope.users = User.query();
 
         if(!Auth.isAdmin()) {
             $location.path('/');
@@ -14,7 +14,8 @@ angular.module('ursaMajorApp')
             [   'Student',
                 'Review Group 1',
                 'Review Group 2',
-                'Committee Chair'
+                'Committee Chair',
+                'Administrator'
             ];
 
         $scope.role =
@@ -22,17 +23,21 @@ angular.module('ursaMajorApp')
 
 
         $scope.delete = function(user) {
-      User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
-      });
-    };
+            User.remove({ id: user._id });
+
+            angular.forEach($scope.users, function(u, i) {
+                if (u === user) {
+                $scope.users.splice(i, 1);
+                }
+            });
+        };
 
         $scope.changeRole = function(user) {
             console.log(user);
-            if(confirm('Are you sure you want to update this users role?')) {
+
+            if (Auth.getCurrentUser().email === user.email) {
+                alert('An administrator must have another administrator change their role to ensure their is always at least one administrator. ');
+            } else if(confirm('Are you sure you want to update this users role?')) {
                 Auth.changeRole(user.role, user);
             };
         };
