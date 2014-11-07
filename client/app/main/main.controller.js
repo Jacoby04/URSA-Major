9302f9a9ffd0186681dcf5c70cef2cfb2ec73a2b
thele116 <thele116@morris.umn.edu>
2014-11-06 21:15:33 -0600

    Fixed misc. bugs with isntEmpty filter. Added filters for submissions without a review group, as well as proof of concept filter for submissions with a specific review group.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,19 +3,5 @@
 angular.module('ursaMajorApp')
   .controller('MainCtrl', function ($scope, $http, Auth) {
       $scope.isLoggedIn = Auth.isLoggedIn;
-        
-//
-//    $scope.jsonSource = "https://spreadsheets.google.com/feeds/list/1ImSQ0fy65Bc9NjmgHrpruaDrodC2uJ1n4RYl2OTX9Po/od6/public/values?alt=json";
-//    $scope.localData = [];
-//
-//    $scope.updateLocalData = function(){
-//        $http({method:'GET', url: $scope.jsonSource}).success(function(data) {
-//            angular.forEach(data.feed.entry, function(value){
-//                $scope.localData.push(value);
-//            });
-//        });
-//    };
-//
-//    $scope.updateLocalData();
 
   });
